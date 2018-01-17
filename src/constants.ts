6dca370f7eea218e17a6e7b4ae438deab0dca092
Viet Truong <viettruong2194@gmail.com>
2018-01-17 00:04:36 -0800

    Remove unused file

--- src/constants.ts
+++
@@ -1,8 +0,0 @@
-export const BACKGROUND_COLOR = <string> '#000';
-export const GAME_OBJECT_COLOR = <string> '#fff';
-export const NORMALIZED_KEYS_MAP = {
-  ArrowUp: 'up',
-  w: 'up',
-  ArrowDown: 'down',
-  s: 'down'
-};
